843e28f228ae9d2e53e149ab514e614969881228
Sp-Sayan <131338948+Sp-Sayan@users.noreply.github.com>
2023-12-31 13:41:25 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React, { createContext, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import LangSelect from "./components/LangSelect/LangSelect";
-import { TextContext } from "./components/Translation/Translation";
+import Preview_Page from "./components/Preview_Page/Preview_Page";
 
-
+export const Preview = React.createContext();
 function App() {
-
+  const [isPreview, setIsPreview] = useState(false);
   return (
-    <div className="container">
-
-      <Header />
-      <LangSelect />
-
+    <div>
+      <Preview.Provider value={[isPreview, setIsPreview]}>
+        {isPreview ? (
+          <Preview_Page />
+        ) : (
+          <div className="container">
+            <Header />
+            <LangSelect />
+          </div>
+        )}
+      </Preview.Provider>
     </div>
   );
 }
